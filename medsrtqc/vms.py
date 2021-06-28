76b7a81ffe8640747e8ab69a0a5fc1783cc0b106
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-28 09:24:31 -0300

    untested stuct defs for types

--- medsrtqc/vms.py
+++ medsrtqc/vms.py
@@ -121,5 +121,111 @@ class VMSReal4(VMSPythonStructField):
         super().__init__('>f')
 
 
-class VMSOceanProcessingProfileType(VMSStruct):
-    pass
+class VMSPrProfileFxd(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('MKEY', VMSCharacter(8)),
+            ('ONE_DEG_SQ', VMSInteger4()),
+            ('CR_NUMBER', VMSCharacter(10)),
+            ('OBS_YEAR', VMSCharacter(4)),
+            ('OBS_DAY', VMSCharacter(2)),
+            ('OBS_TIME', VMSCharacter(4)),
+            ('DATA_TYPE', VMSCharacter(2)),
+            ('IUMSGNO', VMSInteger4()),
+            ('PROF_TYPE', VMSCharacter(4)),
+            ('PROFILE_SEG', VMSCharacter(2)),
+            ('NO_DEPTHS', VMSInteger2()),
+            ('D_P_CODE', VMSCharacter(1)),
+            VMSPadding(1)
+        )
+
+
+class VMSPrProfileProf(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('DEPTH_PRESS', VMSReal4()),
+            ('DP_FLAG', VMSCharacter(1)),
+            ('PARM', VMSReal4()),
+            ('Q_PARM', VMSCharacter(1))
+        )
+
+
+class VMSPrStnFxd(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('MKEY', VMSCharacter(8)),
+            ('ONE_DEG_SQ', VMSInteger4()),
+            ('CR_NUMBER', VMSCharacter(10)),
+            ('OBS_YEAR', VMSCharacter(4)),
+            ('OBS_MONTH', VMSCharacter(2)),
+            ('OBS_DAY', VMSCharacter(2)),
+            ('OBS_TIME', VMSCharacter(4)),
+            ('DATA_TYPE', VMSCharacter(2)),
+            ('IUMSGNO', VMSInteger4()),
+            ('STREAM_SOURCE', VMSCharacter(1)),
+            ('U_FLAG', VMSCharacter(1)),
+            ('STN_NUMBER', VMSInteger2()),
+            ('LATITUDE', VMSReal4()),
+            ('LONGITUDE', VMSReal4()),
+            ('Q_POS', VMSCharacter(1)),
+            ('Q_DATE_TIME', VMSCharacter(1)),
+            ('Q_RECORD', VMSCharacter(1)),
+            ('UP_DATE', VMSCharacter(8)),
+            ('BUL_TIME', VMSCharacter(12)),
+            ('BUL_HEADER', VMSCharacter(6)),
+            ('SOURCE_ID', VMSCharacter(4)),
+            ('STREAM_IDENT', VMSCharacter(4)),
+            ('QC_VERSION', VMSCharacter(4)),
+            ('AVAIL', VMSCharacter(1)),
+            ('NO_PROF', VMSInteger2()),
+            ('NPARMS', VMSInteger2()),
+            ('SPARMS', VMSInteger2()),
+            ('NUM_HISTS', VMSInteger2())
+        )
+
+
+class VMSPrStnProf(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('NO_SEG', VMSInteger2()),
+            ('PROF_TYPE', VMSCharacter(4)),
+            ('DUP_FLAG', VMSCharacter(1)),
+            ('DIGIT_CODE', VMSCharacter(1)),
+            ('STANDARD', VMSCharacter(1)),
+            ('DEEP_DEPTH', VMSReal4()),
+            VMSPadding(1)
+        )
+
+
+class VMSPrStnSurface(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('PCODE', VMSCharacter(4)),
+            ('PARM', VMSReal4()),
+            ('Q_PARM', VMSCharacter(1)),
+            VMSPadding(1)
+        )
+
+
+class VMSPrStnSurfCodes(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('PCODE', VMSCharacter(4)),
+            ('CPARM', VMSCharacter(10)),
+            ('Q_PARM', VMSCharacter(1)),
+            VMSPadding(1)
+        )
+
+
+class VMSPrStnHistory(VMSStruct):
+    def __init__(self) -> None:
+        super().__init__(
+            ('IDENT_CODE', VMSCharacter(2)),
+            ('PRC_CODE', VMSCharacter(4)),
+            ('VERSION', VMSCharacter(4)),
+            ('PRC_DATE', VMSInteger4()),
+            ('ACT_CODE', VMSCharacter(2)),
+            ('ACT_PARM', VMSCharacter(4)),
+            ('AUX_ID', VMSReal4()),
+            ('O_VALUE', VMSReal4())
+        )
