697c99c82b9c91e9b1466f1a2ee539357e33aae4
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-06-28 14:10:55 -0300

    reorganize

--- tests/test_vms.py
+++ tests/test_vms.py
@@ -1,77 +1,77 @@
 
 import unittest
 from io import BytesIO
-from medsrtqc import vms
+from medsrtqc.vms import field
 
 class TestVMSField(unittest.TestCase):
 
     def test_padding(self):
-        field = vms.VMSPadding(3)
-        self.assertEqual(field.n_bytes(), 3)
+        f = field.VMSPadding(3)
+        self.assertEqual(f.n_bytes(), 3)
         file = BytesIO()
-        field.to_stream(file, None)
+        f.to_stream(file, None)
         self.assertEqual(file.getvalue(), b'\x00\x00\x00')
-        self.assertEqual(field.from_stream(BytesIO(b'\x00\x00\x00')), None)
+        self.assertEqual(f.from_stream(BytesIO(b'\x00\x00\x00')), None)
 
     def test_character(self):
-        field = vms.VMSCharacter(5)
-        self.assertEqual(field.n_bytes(), 5)
+        f = field.VMSCharacter(5)
+        self.assertEqual(f.n_bytes(), 5)
         file = BytesIO()
-        field.to_stream(file, 'abcd')
+        f.to_stream(file, 'abcd')
         self.assertEqual(file.getvalue(), b'abcd\x00')
-        self.assertEqual(field.from_stream(BytesIO(b'abcd\x00')), 'abcd')
+        self.assertEqual(f.from_stream(BytesIO(b'abcd\x00')), 'abcd')
 
     def test_struct(self):
-        field = vms.VMSStruct(
-            ('name1', vms.VMSCharacter(3)),
-            vms.VMSPadding(1),
-            ('name2', vms.VMSCharacter(4))
+        f = field.VMSStruct(
+            ('name1', field.VMSCharacter(3)),
+            field.VMSPadding(1),
+            ('name2', field.VMSCharacter(4))
         )
-        self.assertEqual(field.n_bytes(), 8)
+        self.assertEqual(f.n_bytes(), 8)
         file = BytesIO()
-        field.to_stream(file, {'name1': 'abc', 'name2': 'abcd'})
+        f.to_stream(file, {'name1': 'abc', 'name2': 'abcd'})
         self.assertEqual(file.getvalue(), b'abc\x00abcd')
-        self.assertEqual(field.from_stream(BytesIO(b'abc\x00abcd')), {'name1': 'abc', 'name2': 'abcd'})
+        self.assertEqual(f.from_stream(BytesIO(b'abc\x00abcd')), {'name1': 'abc', 'name2': 'abcd'})
 
     def test_array(self):
-        field = vms.VMSArrayOf(vms.VMSCharacter(5), max_length=10)
-        self.assertEqual(field.n_bytes([None]), 5)
+        f = field.VMSArrayOf(field.VMSCharacter(5), max_length=10)
+        self.assertEqual(f.n_bytes([None]), 5)
         file = BytesIO()
-        field.to_stream(file, ['abcd'])
+        f.to_stream(file, ['abcd'])
         self.assertEqual(file.getvalue(), b'abcd\x00')
-        self.assertEqual(field.from_stream(BytesIO(b'abcd\x00'), [None]), ['abcd'])
+        self.assertEqual(f.from_stream(BytesIO(b'abcd\x00'), [None]), ['abcd'])
 
     def test_python_struct(self):
-        field = vms.VMSPythonStructField('>h')
-        self.assertEqual(field.n_bytes(), 2)
+        f = field.VMSPythonStructField('>h')
+        self.assertEqual(f.n_bytes(), 2)
         file = BytesIO()
-        field.to_stream(file, 16)
+        f.to_stream(file, 16)
         self.assertEqual(file.getvalue(), b'\x00\x10')
-        self.assertEqual(field.from_stream(BytesIO(b'\x00\x10')), 16)
+        self.assertEqual(f.from_stream(BytesIO(b'\x00\x10')), 16)
 
     def test_integer2(self):
-        field = vms.VMSInteger2()
-        self.assertEqual(field.n_bytes(), 2)
+        f = field.VMSInteger2()
+        self.assertEqual(f.n_bytes(), 2)
         file = BytesIO()
-        field.to_stream(file, 16)
+        f.to_stream(file, 16)
         self.assertEqual(file.getvalue(), b'\x00\x10')
-        self.assertEqual(field.from_stream(BytesIO(b'\x00\x10')), 16)
+        self.assertEqual(f.from_stream(BytesIO(b'\x00\x10')), 16)
 
     def test_integer4(self):
-        field = vms.VMSInteger4()
-        self.assertEqual(field.n_bytes(), 4)
+        f = field.VMSInteger4()
+        self.assertEqual(f.n_bytes(), 4)
         file = BytesIO()
-        field.to_stream(file, 16)
+        f.to_stream(file, 16)
         self.assertEqual(file.getvalue(), b'\x00\x00\x00\x10')
-        self.assertEqual(field.from_stream(BytesIO(b'\x00\x00\x00\x10')), 16)
+        self.assertEqual(f.from_stream(BytesIO(b'\x00\x00\x00\x10')), 16)
 
     def test_real4(self):
-        field = vms.VMSReal4()
-        self.assertEqual(field.n_bytes(), 4)
+        f = field.VMSReal4()
+        self.assertEqual(f.n_bytes(), 4)
         file = BytesIO()
-        field.to_stream(file, 1)
+        f.to_stream(file, 1)
         self.assertEqual(file.getvalue(), b'?\x80\x00\x00')
-        self.assertEqual(field.from_stream(BytesIO(b'?\x80\x00\x00')), 1)
+        self.assertEqual(f.from_stream(BytesIO(b'?\x80\x00\x00')), 1)
 
 
 class TestOceanProcessingProfile(unittest.TestCase):
