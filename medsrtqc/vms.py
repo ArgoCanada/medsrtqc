a66997f2fcd2bf1063b4f57234ce9fef6c148ca7
Dewey Dunnington <dewey@fishandwhistle.net>
2021-06-27 21:14:40 -0300

    implement and test packing/unpacking of basic types

--- medsrtqc/vms.py
+++ medsrtqc/vms.py
@@ -1,19 +1,41 @@
 
+from io import BytesIO
+from typing import BinaryIO
+from struct import pack, unpack, calcsize
+from collections import OrderedDict
+
+
 class VMSFieldType:
 
     def n_bytes(self):
         pass
 
-    def from_bytes(self, obj: bytes):
+    def from_stream(self, file: BinaryIO):
         pass
 
-    def to_bytes(self, obj) -> bytes:
+    def to_stream(self, file: BinaryIO, value):
         pass
+
+
+class VMSPadding(VMSFieldType):
+
+    def __init__(self, length) -> None:
+        self._length = length
+    
+    def n_bytes(self):
+        return self._length
+
+    def from_stream(self, file: BinaryIO):
+        file.read(self._length)
+        return None
+
+    def to_stream(self, file: BinaryIO, value=None):
+        file.write(b'\x00' * self._length)
 
 
 class VMSCharacter(VMSFieldType):
 
-    def __init__(self, length, encoding='utf-8', pad=b' '):
+    def __init__(self, length, encoding='utf-8', pad=b'\x00'):
         self._length = length
         self._encoding = encoding
         self._pad = pad
@@ -21,33 +43,83 @@ class VMSCharacter(VMSFieldType):
     def n_bytes(self):
         return self._length
 
-    def from_bytes(self, obj: bytes):
-        return obj.decode(self._encoding)
+    def from_stream(self, file: BytesIO):
+        encoded = file.read(self._length).rstrip(self._pad)
+        return encoded.decode(self._encoding)
 
-    def to_bytes(self, obj) -> bytes:
-        encoded = str(obj).encode(self._encoding)
+    def to_stream(self, file: BytesIO, value) -> bytes:
+        encoded = str(value).encode(self._encoding)
         if len(encoded) <= self._length:
-            encoded.ljust(self._length, self._pad)
+            file.write(encoded.ljust(self._length, self._pad))
         else:
-            msg = f"Can't convert '{obj}' to '{self._encoding}' of <= {self._length} bytes"
+            msg = f"Can't convert '{value}' to '{self._encoding}' of <= {self._length} bytes"
             raise ValueError(msg)
 
 
-class VMSInteger2(VMSFieldType):
-    pass
-
-
-class VMSInteger4(VMSFieldType):
-    pass
-
-
-class VMSFloat4(VMSFieldType):
-    pass
-
-
 class VMSStruct(VMSFieldType):
-    pass
+    
+    def __init__(self, *fields) -> None:
+        self._fields = OrderedDict()
+        n_pad = 0
+        for item in fields:
+            if isinstance(item, VMSPadding):
+                name = '__vms_padding_' + str(n_pad)
+                field = item
+                n_pad += 1
+            else:
+                name, field = item
+            
+            self._fields[name] = field
+    
+    def n_bytes(self):
+        return sum(field.n_bytes() for field in self._fields.values())
+
+    def from_stream(self, file: BinaryIO):
+        result = OrderedDict()
+        for name, field in self._fields.items():
+            if isinstance(field, VMSPadding):
+                field.from_stream(file)
+            else:
+                result[name] = field.from_stream(file)
+        return result
+
+    def to_stream(self, file: BinaryIO, value):
+        for name, field in self._fields.items():
+            if name in value:
+                field.to_stream(file, value[name])
+            else:
+                field.to_stream(file)
 
 
-class OceanProcessingProfile(VMSStruct):
+class VMSPythonStructFieldType(VMSFieldType):
+
+    def __init__(self, format) -> None:
+        self._format = format
+
+    def n_bytes(self):
+        return calcsize(self._format)
+
+    def from_stream(self, file: BinaryIO):
+        return unpack(self._format, file.read(self.n_bytes()))[0]
+
+    def to_stream(self, file: BinaryIO, value):
+        file.write(pack(self._format, value))
+
+
+class VMSInteger2(VMSPythonStructFieldType):
+    def __init__(self) -> None:
+        super().__init__('>h')
+
+
+class VMSInteger4(VMSPythonStructFieldType):
+    def __init__(self) -> None:
+        super().__init__('>i')
+
+
+class VMSFloat4(VMSPythonStructFieldType):
+    def __init__(self) -> None:
+        super().__init__('>f')
+
+
+class VMSOceanProcessingProfileType(VMSStruct):
     pass
--- tests/test_vms.py
+++ tests/test_vms.py
@@ -1,12 +1,75 @@
 
 import unittest
+from io import BytesIO
 from medsrtqc import vms
+
+class TestVMSField(unittest.TestCase):
+
+    def test_padding(self):
+        field = vms.VMSPadding(3)
+        self.assertEqual(field.n_bytes(), 3)
+        file = BytesIO()
+        field.to_stream(file, None)
+        self.assertEqual(file.getvalue(), b'\x00\x00\x00')
+        self.assertEqual(field.from_stream(BytesIO(b'\x00\x00\x00')), None)
+
+    def test_character(self):
+        field = vms.VMSCharacter(5)
+        self.assertEqual(field.n_bytes(), 5)
+        file = BytesIO()
+        field.to_stream(file, 'abcd')
+        self.assertEqual(file.getvalue(), b'abcd\x00')
+        self.assertEqual(field.from_stream(BytesIO(b'abcd\x00')), 'abcd')
+
+    def test_struct(self):
+        field = vms.VMSStruct(
+            ('name1', vms.VMSCharacter(3)),
+            vms.VMSPadding(1),
+            ('name2', vms.VMSCharacter(4))
+        )
+        self.assertEqual(field.n_bytes(), 8)
+        file = BytesIO()
+        field.to_stream(file, {'name1': 'abc', 'name2': 'abcd'})
+        self.assertEqual(file.getvalue(), b'abc\x00abcd')
+        self.assertEqual(field.from_stream(BytesIO(b'abc\x00abcd')), {'name1': 'abc', 'name2': 'abcd'})
+
+    def test_python_struct(self):
+        field = vms.VMSPythonStructFieldType('>h')
+        self.assertEqual(field.n_bytes(), 2)
+        file = BytesIO()
+        field.to_stream(file, 16)
+        self.assertEqual(file.getvalue(), b'\x00\x10')
+        self.assertEqual(field.from_stream(BytesIO(b'\x00\x10')), 16)
+    
+    def test_integer2(self):
+        field = vms.VMSInteger2()
+        self.assertEqual(field.n_bytes(), 2)
+        file = BytesIO()
+        field.to_stream(file, 16)
+        self.assertEqual(file.getvalue(), b'\x00\x10')
+        self.assertEqual(field.from_stream(BytesIO(b'\x00\x10')), 16)
+    
+    def test_integer4(self):
+        field = vms.VMSInteger4()
+        self.assertEqual(field.n_bytes(), 4)
+        file = BytesIO()
+        field.to_stream(file, 16)
+        self.assertEqual(file.getvalue(), b'\x00\x00\x00\x10')
+        self.assertEqual(field.from_stream(BytesIO(b'\x00\x00\x00\x10')), 16)
+
+    def test_float4(self):
+        field = vms.VMSFloat4()
+        self.assertEqual(field.n_bytes(), 4)
+        file = BytesIO()
+        field.to_stream(file, 1)
+        self.assertEqual(file.getvalue(), b'?\x80\x00\x00')
+        self.assertEqual(field.from_stream(BytesIO(b'?\x80\x00\x00')), 1)
 
 
 class TestOceanProcessingProfile(unittest.TestCase):
 
     def test_profile(self):
-        prof = vms.OceanProcessingProfile()
+        prof = vms.VMSOceanProcessingProfileType()
         self.assertEqual(1, 1)
 
 
