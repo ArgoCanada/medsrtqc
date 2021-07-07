b9dcc083fa2e70842ea9b50418e237d627139a15
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-07-07 13:29:23 -0300

    add output example to resources

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -8,7 +8,12 @@ from medsrtqc.resources import resource_path
 class TestResources(unittest.TestCase):
 
     def test_exists(self):
-        res = ['BINARY_VMS.DAT', 'BINARY_VMS.json', 'BR6904117_085.nc']
+        res = [
+            'BINARY_VMS.DAT', 'BINARY_VMS.json',
+            'BR6904117_085.nc', 'R6904117_085.nc',
+            'OUTPUT_RT.DAT', 'OUTPUT_RT.json'
+        ]
+
         for r in res:
             self.assertTrue(os.path.isfile(resource_path(r)))
 
