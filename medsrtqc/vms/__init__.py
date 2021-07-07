51bfec3267078a847218d82e7181c8ab41053050
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-07-07 15:38:49 -0300

    add write profiles to vms doc

--- medsrtqc/vms/__init__.py
+++ medsrtqc/vms/__init__.py
@@ -3,10 +3,11 @@
 The classes in the ``vms`` module make reading binary input files
 exported from a VAX/VMS system more expressive and easier to debug
 without affecting the clarity of real-time processing code. In normal
-usage you should only need :func:`read_vms_profiles`.
+usage you should only need :func:`read_vms_profiles` and
+:func:`write_vms_profiles`.
 """
 
 from .read import read_vms_profiles, write_vms_profiles
 from .core_impl import VMSProfile, VMSProfileList
 
-__all__ = ['read_vms_profiles', 'VMSProfile', 'VMSProfileList']
+__all__ = ['read_vms_profiles', 'write_vms_profiles', 'VMSProfile', 'VMSProfileList']
