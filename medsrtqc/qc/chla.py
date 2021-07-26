5c35812e8898429a6e4c7ad5114c5b876847f38a
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-07-26 10:44:58 -0300

    tiny bit of progress on chla processing

--- medsrtqc/qc/chla.py
+++ medsrtqc/qc/chla.py
@@ -2,19 +2,49 @@
 import numpy as np
 import gsw
 
-from .operation import QCOperation
+from .operation import QCOperation, QCOperationError
 from .flag import Flag
 
 
 class ChlaDarkTest(QCOperation):
 
     def run_impl(self):
-        # need chla
         chla = self.profile['CHLA']
 
-        self.log('Setting initial flags to PROBABLY_BAD')
+        self.log('Setting previously unset flags for CHLA to PROBABLY_BAD')
         Flag.update_safely(chla.qc, to=Flag.PROBABLY_BAD)
 
+        self.log('Setting previously unset flags for CHLA_ADJUSTED to GOOD')
+        Flag.update_safely(chla.adjusted_qc, to=Flag.GOOD)
+
+        self.log('Applying global range test to CHLA')
+        values_outside_range = (chla.value < -0.1) | (chla.value > 100.0)
+        Flag.update_safely(chla.qc, Flag.BAD, values_outside_range)
+        Flag.update_safely(chla.adjusted_qc, Flag.BAD, values_outside_range)
+
+        # the DARK correction is not well-defined yet because it needs some
+        # values from the NetCDF that aren't actually in any NetCDFs yet
+        # these variables are FLOAT_DARK/FLOAT_DARK_QC (which may not exist)
+        # and PRELIM_DARK (which stores the iDARK values until there are 5, at which
+        # point the FLOAT_DARK/FLOAT_DARK_QC is calculated)
+        self.log('Checking for previous FLOAT_DARK, FLOAT_DARK_QC, and PRELIM_DARK')
+
+        # the mixed layer depth calculation can fail
+        mixed_layer_depth = None
+        try:
+            mixed_layer_depth = self.mixed_layer_depth()
+        except QCOperationError as e:
+            self.log(e)
+
+        if mixed_layer_depth is not None:
+            self.log(f"Mixed layer depth calculated ({mixed_layer_depth} dbar)")
+
+        # here is where we'd apply the non-photochemical quenching (NPC) correction
+
+        # update the CHLA trace
+        self.update_trace('CHLA', chla)
+
+    def mixed_layer_depth(self):
         self.log('Calculating mixed layer depth')
         pres = self.profile['PRES']
         temp = self.profile['TEMP']
@@ -46,5 +76,4 @@ class ChlaDarkTest(QCOperation):
         with self.pyplot() as plt:
             plt.gca().axhline(y = mixed_layer_depth, linestyle='--')
 
-        # update the CHLA trace
-        self.update_trace('CHLA', chla)
+        return mixed_layer_depth
