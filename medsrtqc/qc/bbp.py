d4466767548118a693deecd0578cb5ab92f987e9
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-03-30 22:46:27 -0300

    copy running median from chla

--- medsrtqc/qc/bbp.py
+++ medsrtqc/qc/bbp.py
@@ -10,3 +10,14 @@ class bbpSpikeTest(QCOperation):
         # global range test will be within this method as well
         bbp = self.profile['BBP']
         self.update_trace('BBP', bbp)
+
+    def running_median(self, n):
+        self.log(f'Calculating running median over window size {n}')
+        x = self.profile['BBP']
+        ix = np.arange(n) + np.arange(len(x)-n+1)[:,None]
+        b = [row[row > 0] for row in x[ix]]
+        k = int(n/2)
+        med = [np.median(c) for c in b]
+        med = np.array(k*[np.nan] + med + k*[np.nan])
+        return med
+
