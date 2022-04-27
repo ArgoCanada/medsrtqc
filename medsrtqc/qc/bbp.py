5aaff21ba40e53a634b9131ea01832f2ef0ebbf6
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-04-27 15:27:37 -0300

    range check, closes #11

--- medsrtqc/qc/bbp.py
+++ medsrtqc/qc/bbp.py
@@ -7,11 +7,20 @@ from .flag import Flag
 class bbpSpikeTest(QCOperation):
 
     def run_impl(self):
-        # global range test will be within this method as well
+        beta = self.profile['BETA_BACKSCATTERING']
         bbp = self.profile['BBP']
-        self.update_trace('BBP', bbp)
 
-        bbp = self.profile['BBP']
+        # get wavelength
+        # wavelength = get wavelength, similar to dark counts or scale
+        wavelength = 700 # dummy placeholder, also will probably always be 700nm
+
+        if wavelength == 532:
+            lower_lim = -0.000005
+        elif wavelength == 700:
+            lower_lim = -0.000025
+        else:
+            self.log(f'No valid wavelength provided (wavelength = {wavelength:d}), setting lower limit of range check to -0.000025')
+            lower_lim = -0.000025
 
         # NOTE this is copied from CHLA, verify in manual that this is proper flagging
         self.log('Setting previously unset flags for BBP to PROBABLY_BAD')
@@ -19,6 +28,12 @@ class bbpSpikeTest(QCOperation):
 
         self.log('Setting previously unset flags for BBP_ADJUSTED to GOOD')
         Flag.update_safely(bbp.adjusted_qc, to=Flag.GOOD)
+
+        # global range test
+        self.log('Applying global range test to BBP')
+        values_outside_range = (bbp.value < lower_lim) | (bbp.value > 0.1)
+        Flag.update_safely(bbp.qc, Flag.BAD, values_outside_range)
+        Flag.update_safely(bbp.adjusted_qc, Flag.BAD, values_outside_range)
 
     def running_median(self, n):
         self.log(f'Calculating running median over window size {n}')
