7aea939201c23ba4fde45819b0d46473f4df7ca2
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-09-07 13:11:43 -0300

    some comment and runs

--- realistic_rtqc_script.py
+++ realistic_rtqc_script.py
@@ -6,19 +6,22 @@ from medsrtqc.vms import read_vms_profiles, write_vms_profiles
 from medsrtqc.qc.chla import ChlaTest
 from medsrtqc.qc.bbp import bbpTest
 
+# more likely open('logs/f{date}_log.log')
 log_file = open('realistic_log.log', 'w')
 
+# read from command line
 vms_file = sys.argv[1]
-print(vms_file)
 profs = read_vms_profiles(vms_file)
 
+# run tests on chlorophyll and bbp
 chla_test = ChlaTest()
 bbp_test  = bbpTest()
-
 for p in profs:
     with contextlib.redirect_stderr(log_file):
         chla_test.run(p)
         bbp_test.run(p)
 
+# export profiles with altered flags, CHLA_ADJUSTED likely populated
 f = open('output_file.dat', 'wb')
 write_vms_profiles(profs, f)
+f.close()
