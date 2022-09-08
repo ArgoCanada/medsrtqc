3c2dc19e9e917c81bb2270d66afa8813ef60fd70
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-09-08 13:26:39 -0300

    for running on meds server

--- realistic_rtqc_script.py
+++ realistic_rtqc_script.py
@@ -8,20 +8,20 @@ from medsrtqc.qc.bbp import bbpTest
 
 # more likely open('logs/f{date}_log.log')
 log_file = open('realistic_log.log', 'w')
+with contextlib.redirect_stderr(log_file):
 
-# read from command line
-vms_file = sys.argv[1]
-profs = read_vms_profiles(vms_file)
+    # read from command line
+    vms_file = sys.argv[1]
+    profs = read_vms_profiles(vms_file)
 
-# run tests on chlorophyll and bbp
-chla_test = ChlaTest()
-bbp_test  = bbpTest()
-for p in profs:
-    with contextlib.redirect_stderr(log_file):
+    # run tests on chlorophyll and bbp
+    chla_test = ChlaTest()
+    bbp_test  = bbpTest()
+    for p in profs:
         chla_test.run(p)
         bbp_test.run(p)
 
-# export profiles with altered flags, CHLA_ADJUSTED likely populated
-f = open('output_file.dat', 'wb')
-write_vms_profiles(profs, f)
-f.close()
+    # export profiles with altered flags, CHLA_ADJUSTED likely populated
+    f = open('output_file.dat', 'wb')
+    write_vms_profiles(profs, f)
+    f.close()
---
+++ run_realistic_script.bat
@@ -0,0 +1,5 @@
+e:
+cd "\Batman Apps\Argo BGC RTQC Test\"
+set filename=.\medsrtqc\resources\bgc_vms.dat
+C:\PROGRA~1\Python\python.exe "e:\Batman Apps\ARGO BGC RTQC Test\realistic_rtqc_script.py" %filename%
+exit
