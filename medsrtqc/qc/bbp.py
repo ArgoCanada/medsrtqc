7ad38fbc7a281bf73aad852ac0992a4e33e8d760
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-09-15 13:29:59 -0300

    range test should change to probably bad, not bad

--- medsrtqc/qc/bbp.py
+++ medsrtqc/qc/bbp.py
@@ -9,7 +9,7 @@ class bbpTest(QCOperation):
     def run_impl(self):
         # get sensor constants
         # wavelength = get wavelength, similar to dark counts or scale
-        wavelength = 700 # dummy placeholder, also will probably always be 700nm
+        # wavelength = 700 # dummy placeholder, also will probably always be 700nm
 
         # convert sensor value to backscatter
         bbp = self.profile['BBP$']
@@ -27,14 +27,14 @@ class bbpTest(QCOperation):
         self.log('Setting previously unset flags for BBP to GOOD')
         Flag.update_safely(bbp.qc, to=Flag.GOOD)
 
-        self.log('Setting previously unset flags for BBP_ADJUSTED to GOOD')
-        Flag.update_safely(bbp.adjusted_qc, to=Flag.GOOD)
+        # self.log('Setting previously unset flags for BBP_ADJUSTED to GOOD')
+        # Flag.update_safely(bbp.adjusted_qc, to=Flag.GOOD)
 
         # global range test
         self.log('Applying global range test to BBP')
         values_outside_range = (bbp.value < lower_lim) | (bbp.value > 0.1)
-        Flag.update_safely(bbp.qc, Flag.BAD, values_outside_range)
-        Flag.update_safely(bbp.adjusted_qc, Flag.BAD, values_outside_range)
+        Flag.update_safely(bbp.qc, Flag.PROBABLY_BAD, values_outside_range)
+        # Flag.update_safely(bbp.adjusted_qc, Flag.PROBABLY_BAD, values_outside_range)
 
         # BBP spike test
         self.log('Performing negative spike test')
@@ -42,7 +42,7 @@ class bbpTest(QCOperation):
         res = bbp.value - median_bbp
         spike_values = res < 2*np.percentile(res, 10)
         Flag.update_safely(bbp.qc, Flag.BAD, spike_values)
-        Flag.update_safely(bbp.adjusted_qc, Flag.BAD, spike_values)
+        # Flag.update_safely(bbp.adjusted_qc, Flag.BAD, spike_values)
 
         self.update_trace('BBP$', bbp)
 
