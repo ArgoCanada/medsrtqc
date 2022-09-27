bf905a5c5fa6a93cd274f199bcc6e322abc2b1f3
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-09-27 17:57:33 -0300

    flag.Flag like structuring for QC tests

--- medsrtqc/history.py
+++ medsrtqc/history.py
@@ -1,8 +1,36 @@
 
 from collections import OrderedDict
+import numpy as np
+
+def read_qc_hex(hex_code):
+    num = int(hex_code, 16)
+    # list to save test number in
+    tests = []
+    for i in range(63, 56, -1):
+        qc_binary_id = 2**i
+        if qc_binary_id <= num:
+            num -= qc_binary_id
+            tests.append(i)
+
+    for i in range(26, 0, -1):
+        qc_binary_id = 2**i
+        if qc_binary_id <= num:
+            num -= qc_binary_id
+            tests.append(i)
+    
+    if num != 0:
+        raise ValueError('Invalid input, decoding QC tests left a non-zero remainder')
+
+    return tests[::-1]
+
+def test_index(test):
+
+    test_numbers = list(range(1, 24)) + list(range(57, 64))
+    return test_numbers.index(test)
 
 class QCx:
 
+    @staticmethod
     def blank(v):
 
         history_qctest = OrderedDict(
@@ -12,3 +40,61 @@ class QCx:
         )
 
         return history_qctest
+
+    @staticmethod
+    def to_array(qc):
+        # hex to numeric
+        tests = read_qc_hex(qc)
+        test_indices = [i-1 for i in tests]
+
+        QC_array = np.zeros((30,))
+        QC_array[test_indices] = 1
+
+    @staticmethod
+    def update_safely(qc, test, passfail):
+        qcp = 0
+        qcf = 1
+        ix = test_index(test)
+        if passfail in ['p', 'P', 'PASS', 'pass', 1, '1']:
+            if qc[qcp, ix] == 0 and qc[qcf, ix] == 0:
+                qc[qcp, ix] = 1
+        
+        if passfail in ['f', 'fail', 'F', 'FAIL', 0, '0']:
+            if qc[qcp, ix] == 0 and qc[qcf, ix] == 0:
+                qc[qcf, ix] = 1
+            elif qc[qcp, ix] == 1:
+                qc[qcf, ix] = 1
+                qc[qcp, ix] = 0
+
+    test_descriptions = [
+        '1. Platform Identification test',
+        '2. Impossible Date test',
+        '3. Impossible Location test',
+        '4. Position on Land test',
+        '5. Impossible Speed test',
+        '6. Global Range test',
+        '7. Regional Global Parameter test',
+        '8. Pressure Increasing test',
+        '9. Spike test',
+        '10. Top and Bottom Spike test (obsolete)',
+        '11. Gradient test',
+        '12. Digit Rollover test',
+        '13. Stuck Value test',
+        '14. Density Inversion test',
+        '15. Grey List test',
+        '16. Gross Salinity or Temperature Sensor Drift test',
+        '17. Visual QC test',
+        '18. Frozen profile test',
+        '19. Deepest pressure test',
+        '20. Questionable Argos position test',
+        '21. Near-surface unpumped CTD salinity test',
+        '22. Near-surface mixed air/water test',
+        '23. Real-time Quality Control Flag Scheme for float data deeper than 2000 dbar',
+        '57. DOXY specific test',
+        '58. CDOM specific test',
+        '59. NITRATE specific test',
+        '60. PAR specific test',
+        '61. IRRADIANCE specific test',
+        '62. BBP specific tests',
+        '63. CHLA specific tests',
+    ]
