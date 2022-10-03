72f8cdb8425035b9bb835058535ec1b64c904cfb
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-10-03 20:54:44 -0300

    changes to rtqc script

--- realistic_rtqc_script.py
+++ realistic_rtqc_script.py
@@ -1,27 +1,30 @@
 
 import sys
+from datetime import datetime
 import contextlib
 
 from medsrtqc.vms import read_vms_profiles, write_vms_profiles
 from medsrtqc.qc.check import preTestCheck
 
-# more likely open('logs/f{date}_log.log')
-log_file = open('realistic_log.log', 'w')
+log_file = open(f'logs/{datetime.utcnow().strftime("%Y%m%d_%H%M")}_log.log')
 with contextlib.redirect_stderr(log_file):
 
     # read from command line
-    vms_file = sys.argv[1]
-    profs = read_vms_profiles(vms_file)
+    input_file = sys.argv[1]
+    profs = read_vms_profiles(input_file)
 
-    # run tests on chlorophyll and bbp
+    # run tests on appropriate bgc variables
     check = preTestCheck()
     for p in profs:
+        # add FLUA if appropriate, and QCP/QCF variables if they don't exist
         p.prepare()
+        # which tests to do based on variables in profile
         tests = check.run(p)
         for t in tests:
             t.run(p)
 
     # export profiles with altered flags, CHLA_ADJUSTED likely populated
-    f = open('output_file.dat', 'wb')
+    output_file = input_file.replace('.dat', '_output.dat')
+    f = open(output_file, 'wb')
     write_vms_profiles(profs, f, ver=2)
     f.close()
