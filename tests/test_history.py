c50108ee22b43e076c811e1ef8989c5c8583b84d
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2022-10-25 12:09:33 -0300

    use test index instead of hard coding

--- tests/test_history.py
+++ tests/test_history.py
@@ -10,20 +10,20 @@ class TestHistory(unittest.TestCase):
         self.assertEqual(tests, [4, 63])
 
         qc_arr = hist.qc_array(hexval)
-        self.assertTrue(qc_arr[3] == 1)
+        self.assertTrue(qc_arr[hist.test_index(4)] == 1)
 
         fail = hex(2**62 + 2**9)
         qcpf = hist.QCx.qc_tests(hexval, fail)
 
         hist.QCx.update_safely(qcpf, 8, 'pass')
-        self.assertEqual(qcpf[0, 7], 1)
+        self.assertEqual(qcpf[0, hist.test_index(8)], 1)
 
         hist.QCx.update_safely(qcpf, 2, 'fail')
-        self.assertEqual(qcpf[1, 3], 1)
+        self.assertEqual(qcpf[1, hist.test_index(2)], 1)
 
         hist.QCx.update_safely(qcpf, 4, 'fail')
-        self.assertEqual(qcpf[0, 3], 0)
-        self.assertEqual(qcpf[1, 3], 1)
+        self.assertEqual(qcpf[0, hist.test_index(4)], 0)
+        self.assertEqual(qcpf[1, hist.test_index(4)], 1)
 
 if __name__ == '__main__':
     unittest.main()
