26e49b91928f57910322c90d2c9de242aa6b6711
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2024-01-04 13:27:42 -0400

    comment old tests
    keep as comments for posterity for now

--- medsrtqc/qc/bbp.py
+++ medsrtqc/qc/bbp.py
@@ -85,39 +85,39 @@ class bbpTest(QCOperation):
                         Flag.update_safely(bbp.qc, Flag.BAD, where=deep_above_baseline)
                         self.log(f'Parking hook test results: {sum(deep_above_baseline)} points set to 4')
                     
-        # old tests - still run or no?
+        # # old tests - still run or no?
 
-        if 'B700' in self.profile.keys() or 'BBP700' in self.profile.keys():
-            lower_lim = -0.000025
-        elif 'B532' in self.profile.keys() or 'BBP532' in self.profile.keys(): # pragma: no cover
-            lower_lim = -0.000005
-        else: # pragma: no cover
-            self.log(f'No valid wavelength information found, setting lower limit of range check to -0.000025')
-            lower_lim = -0.000025
+        # if 'B700' in self.profile.keys() or 'BBP700' in self.profile.keys():
+        #     lower_lim = -0.000025
+        # elif 'B532' in self.profile.keys() or 'BBP532' in self.profile.keys(): # pragma: no cover
+        #     lower_lim = -0.000005
+        # else: # pragma: no cover
+        #     self.log(f'No valid wavelength information found, setting lower limit of range check to -0.000025')
+        #     lower_lim = -0.000025
 
-        # global range test
-        self.log('Applying global range test to BBP')
-        values_outside_range = (bbp.value < lower_lim) | (bbp.value > 0.1)
-        Flag.update_safely(bbp.qc, Flag.PROBABLY_BAD, values_outside_range)
-        QCx.update_safely(self.profile.qc_tests, 6, not any(values_outside_range))
-        all_passed = all_passed and not any(values_outside_range)
+        # # global range test
+        # self.log('Applying global range test to BBP')
+        # values_outside_range = (bbp.value < lower_lim) | (bbp.value > 0.1)
+        # Flag.update_safely(bbp.qc, Flag.PROBABLY_BAD, values_outside_range)
+        # QCx.update_safely(self.profile.qc_tests, 6, not any(values_outside_range))
+        # all_passed = all_passed and not any(values_outside_range)
 
-        # BBP spike test
-        self.log('Performing negative spike test')
-        median_bbp = self.running_median(5)
-        res = bbp.value - median_bbp
-        spike_values = res < 2*np.percentile(res, 10)
-        Flag.update_safely(bbp.qc, Flag.BAD, spike_values)
-        all_passed = all_passed and not any(spike_values)
-        QCx.update_safely(self.profile.qc_tests, 9, not any(spike_values))
+        # # BBP spike test
+        # self.log('Performing negative spike test')
+        # median_bbp = self.running_median(5)
+        # res = bbp.value - median_bbp
+        # spike_values = res < 2*np.percentile(res, 10)
+        # Flag.update_safely(bbp.qc, Flag.BAD, spike_values)
+        # all_passed = all_passed and not any(spike_values)
+        # QCx.update_safely(self.profile.qc_tests, 9, not any(spike_values))
 
-        # stuck value test
-        self.log('Performing stuck value test on bbp')
-        stuck_value = all(bbp.value == bbp.value[0])
-        if stuck_value: # pragma: no cover
-            self.log('stuck values found, setting all profile flags to 4')
-            Flag.update_safely(bbp.qc, Flag.BAD)
-        QCx.update_safely(self.profile.qc_tests, 13, not stuck_value)
+        # # stuck value test
+        # self.log('Performing stuck value test on bbp')
+        # stuck_value = all(bbp.value == bbp.value[0])
+        # if stuck_value: # pragma: no cover
+        #     self.log('stuck values found, setting all profile flags to 4')
+        #     Flag.update_safely(bbp.qc, Flag.BAD)
+        # QCx.update_safely(self.profile.qc_tests, 13, not stuck_value)
 
         # update QCP/QCF
         QCx.update_safely(self.profile.qc_tests, 62, all_passed)
