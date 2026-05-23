1b69f7d16eec0a0e3616004bfe992690193935d9
Christopher Gordon <chris.gordon@dfo-mpo.gc.ca>
2026-05-23 17:08:45 -0300

    try to import netCDF4/Dataset

--- scripts/batman_exceptions.py
+++ scripts/batman_exceptions.py
@@ -4,6 +4,7 @@ import traceback
 try:
     import pandas as pd
     from medsrtqc.resources import resource_path
+    from netCDF4 import Dataset
 
     fn = resource_path('CHLA_netCDF_info.csv')
     pd.read_csv(fn)
